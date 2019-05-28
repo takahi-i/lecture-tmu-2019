5127a643d9e756fa8581ec821aeca12751f17417
takahi-i <takahi-i@cookpad.com>
2019-05-28 12:38:57 +0900

    copy later half of columns related to converting data in the notebook

--- scripts/convert_data.py
+++ scripts/convert_data.py
@@ -71,3 +71,36 @@ print(neg_files[0:2])
 print(pos_files[0:2])
 
 print(word_counter("I am YK. I love data analysis using python."))
+
+DATA_NUM = 700
+
+unigrams_data = get_unigram(neg_files[:DATA_NUM]) + get_unigram(pos_files[:DATA_NUM])
+
+print(unigrams_data[0])
+print("data size :", sys.getsizeof(unigrams_data) / 1000000, "[MB]")
+
+vec = DictVectorizer()
+feature_vectors_csr = vec.fit_transform(unigrams_data)
+
+print(feature_vectors_csr)
+
+feature_vectors = vec.fit_transform(unigrams_data).toarray()
+print("data dimension :", feature_vectors.shape)
+print(feature_vectors[0])
+print("data size :", sys.getsizeof(feature_vectors) / 1000000, "[MB]")
+
+labels = np.r_[np.tile(0, DATA_NUM), np.tile(1, DATA_NUM)]
+print(labels[0], labels[DATA_NUM - 1], labels[DATA_NUM], labels[2 * DATA_NUM - 1])
+
+np.random.seed(7789)
+shuffle_order = np.random.choice(2 * DATA_NUM, 2 * DATA_NUM, replace=False)
+
+print("length :", len(shuffle_order))
+print("first 10 elements :", shuffle_order[0:10])
+
+one_third_size = int(2 * DATA_NUM / 3.)
+print("one third of the length :", one_third_size)
+
+print("# of '1' in 1st set :", np.sum(labels[shuffle_order[:one_third_size]]))
+print("# of '1' in 2nd set :", np.sum(labels[shuffle_order[one_third_size:2 * one_third_size]]))
+print("# of '1' in 3rd set :", np.sum(labels[shuffle_order[2 * one_third_size:]]))
