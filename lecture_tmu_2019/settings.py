654f418f318d58f4ab83d486b1cfd72c66a2b3f9
takahi-i <takahi-i@cookpad.com>
2019-09-09 15:37:49 +0900

    Remove functions on cross validation

--- lecture_tmu_2019/settings.py
+++ lecture_tmu_2019/settings.py
@@ -3,4 +3,3 @@ SEARCH_PARAMETERS = [{'kernel': ['rbf'], 'gamma': [1e-2, 1e-3, 1e-4], 'C': [0.1,
 DATASET_BASE_PATH = "dataset"
 POSITIVE_FILES = "{}/tokens/pos/*".format(DATASET_BASE_PATH)
 NEGATIVE_FILES = "{}/tokens/neg/*".format(DATASET_BASE_PATH)
-DATA_NUM = 700
